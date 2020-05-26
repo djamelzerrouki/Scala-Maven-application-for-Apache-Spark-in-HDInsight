483eacbeac73b924f4fd0ace346c9f9869868a6c
zerrouki djamel <djameljimmizerrouki@gmail.com>
2020-05-26 20:54:16 +0200

    max price

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -1,9 +1,18 @@
 package org.example
 
-/**
- * Hello world!
- *
- */
-object App extends Application {
-  println( "Hello World!" )
+import org.apache.spark.SparkContext._
+import org.apache.spark.{SparkConf,SparkContext}
+
+object MaxPrice {
+  def main(args: Array[String]){
+    val conf = new SparkConf().setAppName("Max Price")
+    val sc = new SparkContext(conf)
+
+    sc.textFile(args(0))
+      .map(_.split(","))
+      .map(rec => ((rec(0).split("-"))(0).toInt, rec(1).toFloat))
+      .reduceByKey((a,b) => Math.max(a,b))
+      .saveAsTextFile(args(1))
+
+  }
 }
